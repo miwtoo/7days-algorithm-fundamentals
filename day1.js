8be1feb2829689eecea2fa34c145ec0e06fc6b99
miwtoo <miwyugioh@gmail.com>
2024-12-07 16:13:49 +0700

    refactor: :recycle: day1 fix lint

--- day1.js
+++ day1.js
@@ -1,7 +1,7 @@
 export function findDuplicateNumber(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] == arr[j]) return arr[i]
+            if (arr[i] === arr[j]) return arr[i]
         }
     }
 
